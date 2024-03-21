e03bad6ec318f664e124b676e42c365f295aae53
Ms-Juli <visuallady@mail.ru>
2024-03-21 19:10:45 +0500

    12-step-by-step

--- julia/12-step-by-step/script.js
+++ julia/12-step-by-step/script.js
@@ -1,6 +1,7 @@
 const prevButtonDOM = document.getElementById("prev");
 const nextButtonDOM = document.getElementById("next");
 const circlesDOM = document.querySelectorAll(".circle");
+const lineDOM = document.getElementById("line");
 
 let currentStep = 1;
 
@@ -8,12 +9,31 @@ const render = () => {
     circlesDOM.forEach((circle, index) => {
         index++;
 
-        if (index === currentStep) {
+        if (currentStep >= index) {
             circle.classList.add("active");
         } else {
             circle.classList.remove("active");
         }
-    })
+    });
+
+    if (currentStep > 1) {
+        let width = Math.floor(100 / (circlesDOM.length - 1)) * (currentStep - 1);
+        lineDOM.style.width = `${width}%`;
+    } else {
+        lineDOM.style.width = "0%";
+    }
+
+    if (currentStep === 1) {
+        prevButtonDOM.disabled = true;
+    } else {
+        prevButtonDOM.disabled = false;
+    }
+
+    if (currentStep === circlesDOM.length) {
+        nextButtonDOM.disabled = true;
+    } else {
+        nextButtonDOM.disabled = false;
+    }
 }
 
 prevButtonDOM.addEventListener("click", function () {
@@ -35,5 +55,7 @@ nextButtonDOM.addEventListener("click", function () {
 
     render();
 });
+
+render();
 
 
