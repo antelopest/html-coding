eb8a5e9cebb0b082050ee5689925b63192dd651a
antelopest <antelopest@gmail.com>
2025-06-10 13:57:13 +0700

    main 11. Flexbox, Grid example add script.js

---
+++ lectures/css/11. Frexbox, Grid/examples/gallery/scripts/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const filters = {
+  all: {
+    name: "All",
+    active: true,
+  },
+  nature: {
+    name: "Nature",
+    active: false,
+  },
+  city: {
+    name: "City",
+    active: false,
+  },
+  people: {
+    name: "People",
+    active: false,
+  },
+};
