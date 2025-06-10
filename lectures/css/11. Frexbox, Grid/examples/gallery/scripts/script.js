e8d5971c053c44c3fd8527591b000291fafb90d7
antelopest <antelopest@gmail.com>
2025-06-10 14:03:56 +0700

    main add gallery

--- lectures/css/11. Frexbox, Grid/examples/gallery/scripts/script.js
+++ lectures/css/11. Frexbox, Grid/examples/gallery/scripts/script.js
@@ -1,20 +1,60 @@
 "use strict";
 
+const category = {
+  all: "All",
+  nature: "Nature",
+  city: "City",
+  people: "People",
+};
+
 const filters = {
   all: {
-    name: "All",
+    name: category.all,
     active: true,
   },
   nature: {
-    name: "Nature",
+    name: category.nature,
     active: false,
   },
   city: {
-    name: "City",
+    name: category.city,
     active: false,
   },
   people: {
-    name: "People",
+    name: category.people,
     active: false,
   },
 };
+
+const gallery = [
+  {
+    category: category.nature,
+    url: "https://picsum.photos/id/1015/400/300",
+  },
+  {
+    category: category.city,
+    url: "https://picsum.photos/id/1016/400/300",
+  },
+  {
+    category: category.people,
+    url: "https://picsum.photos/id/1025/400/300",
+  },
+  {
+    category: category.nature,
+    url: "https://picsum.photos/id/1018/400/300",
+  },
+  {
+    category: category.city,
+    url: "https://picsum.photos/id/1020/400/300",
+  },
+  {
+    category: category.people,
+    url: "https://picsum.photos/id/1027/400/300",
+  },
+];
+
+/* DOM ELEMENTS */
+const filtersDOM = document.getElementById("filters");
+const galleryDOM = document.getElementById("gallery");
+
+/* */
