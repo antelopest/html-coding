ca2198e836d1b7e1a0902c67d9e93186b7805e0d
antelopest <antelopest@gmail.com>
2025-06-16 18:24:01 +0700

    main array methods reduce

--- lectures/javascript/2. Array and object/examples/reduce.js
+++ lectures/javascript/2. Array and object/examples/reduce.js
@@ -1,8 +1,16 @@
-const numbers = [1, 2, 3, 4, 5, 6];
+const numbers = [-1, 0, 1, 2, 3, 4, 5, 6];
 
-const sum = numbers.reduce((acc, number) => acc + number, 0);
+const sum = numbers.reduce((acc, n) => acc + n, 0);
 
-console.log(sum);
+console.log("Sum:", sum);
+
+const max = numbers.reduce((max, n) => (n > max ? n : max));
+
+console.log("Max:", max);
+
+const min = numbers.reduce((min, n) => (n < min ? n : min));
+
+console.log("Min:", min);
 
 const Status = {
   online: {
@@ -15,21 +23,35 @@ const Status = {
   },
 };
 
+const Roles = {
+  admin: {
+    id: 1,
+    name: "Admin",
+  },
+  user: {
+    id: 2,
+    name: "User",
+  },
+};
+
 const users = [
   {
     id: 1,
     name: "Samuel",
     status: Status.online,
+    role: Roles.admin,
   },
   {
     id: 2,
     name: "Nataly",
     status: Status.offline,
+    role: Roles.user,
   },
   {
     id: 3,
     name: "Boris",
     status: Status.online,
+    role: Roles.user,
   },
 ];
 
@@ -42,3 +64,12 @@ const activeUsers = users.reduce((activeUsers, user) => {
 }, []);
 
 console.log(activeUsers);
+
+const groupedByRole = users.reduce((acc, user) => {
+  acc[user.role.name] = acc[user.role.name] || [];
+  acc[user.role.name].push(user);
+
+  return acc;
+}, []);
+
+console.log(groupedByRole);
