1e03fb97d3de85b9839babd678e68699c6fe91a9
antelopest <antelopest@gmail.com>
2025-06-17 16:52:19 +0700

    main add file slice.js

---
+++ lectures/javascript/2. Array and object/examples/reverse.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers.reverse()); // [5, 4, 3, 2, 1]
---
+++ lectures/javascript/2. Array and object/examples/slice.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers.slice(0, 2)); // [1,2]
+console.log(numbers.slice(2)); // [3,4,5]
+console.log(numbers.slice(1)); // [2,3,4,5]
